c9f1dcc5f4412438762164e3211dae6cd9693d5b
Alexander Kine <sanya_179510@mail.ru>
2015-03-15 01:14:04 +0500

    сделал отдельно однопоточную реализацию back-tracking алгоритма и тесты на него, сделана многопоточная версия и тесты на корректность и быстродействие

--- JapaneseCrossword/Program.cs
+++ JapaneseCrossword/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace JapaneseCrossword
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using JapaneseCrossword.CrosswordSolvers;
+
+namespace JapaneseCrossword
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var solver = new CrosswordSolver();
-            var status = solver.Solve(@"TestFiles\Car.txt", "output.txt");
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            ICrosswordSolver solver = new MultithreadedCrosswordSolver();
+            var status = solver.Solve(@"TestFiles\Winter.txt", "output1.txt");
+            stopWatch.Stop();
+            Console.WriteLine("  One threaded solver time: {0,3}   Status: {1}", stopWatch.Elapsed.Milliseconds, status);
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            solver = new CrosswordSolver();
+            status = solver.Solve(@"TestFiles\Winter.txt", "output2.txt");
+            stopWatch.Stop();
+            Console.WriteLine("Multi threaded solver time: {0,3}   Status: {1}", stopWatch.Elapsed.Milliseconds, status);
+            Console.WriteLine("\nOutput files contents equals?  {0}", File.ReadAllText("output1.txt").Equals(File.ReadAllText("output2.txt")));
+            Console.ReadKey();
         }
     }
 }
